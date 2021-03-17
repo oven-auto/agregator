e8c8dc51a4abb17e17fc42713859300d7e17aa50
Ivan <wanokazak1990@yandex.ru>
2021-03-17 14:51:08 +0300

    fav

--- app/resources/views/layouts/app.blade.php
+++ app/resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
+    <link rel="icon" type="image/png" href="{{asset('images/fav.png')}}">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
