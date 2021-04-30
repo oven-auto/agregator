a90e03bafb1d36c9aa86928d7858c7f9ffd3f99c
Ivan <wanokazak1990@yandex.ru>
2021-04-30 16:09:09 +0300

    Api

--- app/resources/views/main/nav.blade.php
+++ app/resources/views/main/nav.blade.php
@@ -7,21 +7,37 @@
 
 		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
 		    <ul class="navbar-nav mr-auto">
-		      <li class="nav-item">
-		        <a class="nav-link" href="#salonrenault" style="">Автосалон Renault</a>
+		      <li class="nav-item dropdown">
+		        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		          Автосалон Renault
+		        </a>
+		        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+		          <a class="dropdown-item" href="http://www.renault.oven-auto.ru/content/availablelist">Автомобили в продаже</a>
+		          @if(isset($actionLinks) && isset($actionLinks[1]))
+		          	@foreach($actionLinks[1] as $itemRenalt)
+		          	<a target="_blank" class="nav-link" href="http://promo.oven-auto.ru/content/view?id={{$itemRenalt->id}}" style="">
+			        	{{$itemRenalt->name}}
+			        </a>
+		          	@endforeach
+		          @endif
+		        </div>
 		      </li>
-		      <li class="nav-item">
-		        <a class="nav-link" href="#salonlada" style="">Автосалон Lada</a>
+
+		      <li class="nav-item dropdown">
+		        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		          Автосалон Lada
+		        </a>
+		        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+		          <a class="dropdown-item" href="https://oven-auto.lada.ru/ds/cars/">Автомобили в продаже</a>
+		          @if(isset($actionLinks) && isset($actionLinks[2]))
+		          	@foreach($actionLinks[2] as $itemRenalt)
+		          	<a target="_blank" class="nav-link" href="http://promo.oven-auto.ru/content/view?id={{$itemRenalt->id}}" style="">
+			        	{{$itemRenalt->name}}
+			        </a>
+		          	@endforeach
+		          @endif
+		        </div>
 		      </li>
-		      @if(isset($actionLinks) && count($actionLinks))
-		      	@foreach($actionLinks as $itemLink)
-		      		<li class="nav-item">
-				        <a target="_blank" class="nav-link" href="http://promo.oven-auto.ru/content/view?id={{$itemLink->id}}" style="">
-				        	{{$itemLink->name}}
-				        </a>
-				    </li>
-		      	@endforeach
-		      @endif
 		    </ul>
 		  </div>
 		</div>
